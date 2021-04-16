abff7461d1c04bbf272121a35f5575db6759c1a2
xiaomingTang <1038761793@qq.com>
2021-04-16 13:51:57 +0800

    新增Anchor组件, 为Iconfont组件添加说明

---
+++ template-web-page-react/src/components/Anchor/index.tsx
@@ -0,0 +1,60 @@
+import { joinSpace } from "@Src/utils"
+import React, {
+  AnchorHTMLAttributes, ReactNode, useMemo,
+} from "react"
+
+import Iconfont from "@Src/components/Iconfont"
+
+import Styles from "./index.module.less"
+
+interface AnchorProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  icon?: ReactNode;
+}
+
+export function Anchor({
+  href,
+  target,
+  rel,
+  className,
+  children,
+  icon,
+  ...props
+}: AnchorProps) {
+  const isOuterSite = useMemo(() => {
+    const url = new URL(href || "", window.location.href)
+    return url.hostname !== window.location.hostname
+  }, [href])
+
+  const finalIcon: ReactNode = useMemo(() => {
+    if (icon !== undefined) {
+      return icon
+    }
+    return <sup><Iconfont type={isOuterSite ? "link-outer" : "link-inner"} /></sup>
+  }, [icon, isOuterSite])
+
+  const finalTarget = useMemo(() => {
+    if (target !== undefined) {
+      return target
+    }
+    console.log(isOuterSite ? "_blank" : "_self")
+    return isOuterSite ? "_blank" : "_self"
+  }, [isOuterSite, target])
+
+  const finalRel = useMemo(() => {
+    if (rel !== undefined) {
+      return rel
+    }
+    console.log(isOuterSite ? "noopener noreferrer" : "")
+    return isOuterSite ? "noopener noreferrer" : ""
+  }, [isOuterSite, rel])
+
+  return <a
+    href={href}
+    target={finalTarget}
+    rel={finalRel}
+    className={joinSpace(className, Styles.anchor)}
+    {...props}
+  >
+    {children}{finalIcon}
+  </a>
+}
--- template-web-page-react/src/components/Iconfont/index.tsx
+++ template-web-page-react/src/components/Iconfont/index.tsx
@@ -4,17 +4,33 @@ import { joinSpace } from "@Src/utils"
 import "./iconfont/iconfont.css"
 
 interface Props extends HTMLAttributes<HTMLSpanElement> {
+  /**
+   * 阿里 iconfont 图标名称(无须 iconfont- 前缀)
+   */
   type: string;
 }
 
+/**
+ * 阿里 iconfont 默认会为 .iconfont 类添加 font-size: 16px 样式
+ *
+ * 因此如需设置字体, 需在 style 中覆盖 fontSize
+ *
+ * 本组件默认提供 fontSize: inherit;
+ *
+ * 使用者如有需要可自行覆盖
+ */
 export default function Iconfont({
-  type, className, ...props
+  type, className, style, ...props
 }: Props) {
   return <span
     className={joinSpace(
       `iconfont iconfont-${type}`,
       className,
     )}
+    style={{
+      fontSize: "inherit",
+      ...style,
+    }}
     {...props}
   />
 }
