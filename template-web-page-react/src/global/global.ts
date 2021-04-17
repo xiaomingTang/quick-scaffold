05ad58eed521a79ef6ec9e02e20103d6dcc39dd6
xiaomingTang <1038761793@qq.com>
2021-04-17 17:38:12 +0800

    添加新版本已加载的提示

--- template-web-page-react/src/global/global.ts
+++ template-web-page-react/src/global/global.ts
@@ -9,6 +9,22 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("./service-worker.js").then((registration) => {
       console.log("SW registered: ", registration)
+      registration.addEventListener("updatefound", () => {
+        const newWorker = registration.installing
+        if (newWorker) {
+          newWorker.addEventListener("statechange", () => {
+            if (newWorker.state === "installed") {
+              if (navigator.serviceWorker.controller) {
+                console.log("new version available")
+                // eslint-disable-next-line no-alert
+                if (window.confirm("新版本已准备好, 是否重启以加载新版本？")) {
+                  window.location.reload()
+                }
+              }
+            }
+          })
+        }
+      })
     }).catch((registrationError) => {
       console.error("SW registration failed: ", registrationError)
     })
