b7a37645df5a1337c372185ea5ccd3543a18fb27
xiaomingTang <1038761793@qq.com>
2021-05-23 15:30:55 +0800

    feat: 输出版本信息

--- scripts/__rename.ts
+++ scripts/__rename.ts
@@ -1,10 +1,10 @@
 /**
  * 不知道为什么, 当 dist/index.js 作为 package.json 中 bin 的值
- * 
+ *
  * windows 无视 #!/usr/bin/env node 的声明, 仍然当作 JScript 文件处理, 导致脚本出错
- * 
+ *
  * 后缀不为 .js 则能正确调用 node 来执行脚本
- * 
+ *
  * 所以本文件是专用于将 dist/index.js 重命名为 dist/index (移除 .js 后缀)
  */
 import fs from "fs"
@@ -16,7 +16,7 @@ function rename() {
     fs.renameSync(resolveProject("dist/index.js"), resolveProject("dist/index"))
   } catch (error) {
     log.error("rename error, or you can rename quick-scaffold/dist/index.js to quick-scaffold/dist/index manually")
-  }  
+  }
 }
 
 if (require.main === module) {
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 import { init } from "./commands/init"
+import { log, resolveProject } from "./utils"
 
 async function main() {
   await init()
 }
 
 if (require.main === module) {
+  const { name, version } = require(resolveProject("package.json"))
+  log.success(`${name}: ${version}`)
   main()
 }
