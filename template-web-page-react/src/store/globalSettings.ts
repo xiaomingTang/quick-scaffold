de2fe3e7ee014b415f6b335a3e31f2c7ef37e8c4
xiaomingTang <1038761793@qq.com>
2021-09-10 17:41:29 +0800

    fix: 根据最新的eslint修正switch case indent

--- template-web-page-react/src/store/globalSettings.ts
+++ template-web-page-react/src/store/globalSettings.ts
@@ -33,42 +33,42 @@ export type Action = {
 
 export function reducer(state = initState, action: Action): State {
   switch (action.type) {
-  case "@globalSettings/i18n": {
-    const lang = action.value
-    if (availableLangs.includes(lang)) {
+    case "@globalSettings/i18n": {
+      const lang = action.value
+      if (availableLangs.includes(lang)) {
+        return {
+          ...state,
+          lang: action.value,
+        }
+      }
+      console.error(`wrong language: ${lang}`)
+      return state
+    }
+    case "@globalSettings/transitionType": {
+      const transitionType = action.value
+      if (availableTransitionTypes.includes(transitionType)) {
+        return {
+          ...state,
+          transitionType: action.value,
+        }
+      }
+      console.error(`wrong language: ${transitionType}`)
+      return state
+    }
+    case "@globalSettings/deferredPrompt": {
       return {
         ...state,
-        lang: action.value,
+        deferredPrompt: action.value,
       }
     }
-    console.error(`wrong language: ${lang}`)
-    return state
-  }
-  case "@globalSettings/transitionType": {
-    const transitionType = action.value
-    if (availableTransitionTypes.includes(transitionType)) {
-      return {
-        ...state,
-        transitionType: action.value,
+    default: {
+      // redux 初始化会执行到这儿
+      if (action && !/^@@redux/.test((action as Action).type)) {
+        // 这儿的赋值, 是仅用于类型检查的, 正常是不会执行到这里的
+        const neverAction: never = action
+        console.error("如果执行到这里了, 说明出错了, 可能你新增了 action 类型, 却忘了在 reducer 的 switch case 中处理.\n\nwrong action: ", neverAction)
       }
+      return state
     }
-    console.error(`wrong language: ${transitionType}`)
-    return state
-  }
-  case "@globalSettings/deferredPrompt": {
-    return {
-      ...state,
-      deferredPrompt: action.value,
-    }
-  }
-  default: {
-    // redux 初始化会执行到这儿
-    if (action && !/^@@redux/.test((action as Action).type)) {
-      // 这儿的赋值, 是仅用于类型检查的, 正常是不会执行到这里的
-      const neverAction: never = action
-      console.error("如果执行到这里了, 说明出错了, 可能你新增了 action 类型, 却忘了在 reducer 的 switch case 中处理.\n\nwrong action: ", neverAction)
-    }
-    return state
-  }
   }
 }
